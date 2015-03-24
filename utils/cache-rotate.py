012b4fdd7618a3d3bfe2be083a9f210a75945fb3
Christian Holler (:decoder) <choller@mozilla.com>
2015-03-24 16:08:09 +0100

    Add script to rotate the shell-cache

---
+++ utils/cache-rotate.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import shutil
+from __builtin__ import file
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: %s <path to shell cache>" % sys.argv[0])
+        sys.exit(1)
+    
+    path = sys.argv[1]
+
+    while (getFreeSpacePercentage(path) < 0.05):
+        shellDirs = os.listdir(path)
+        
+        (oldestShellDir, oldestShellDirAtime) = (None, None)
+        
+        for shellDir in shellDirs:
+            binPath = None
+            if shellDir.startswith("tboxjs-"):
+                binPath = os.path.join(path, shellDir, "build", "dist", "js")
+            elif shellDir.startswith("js-"):
+                for file in os.listdir(os.path.join(path, shellDir)):
+                    if file.startswith("js-"):
+                        binPath = os.path.join(path, shellDir, file)
+                        break
+            else:
+                continue
+            
+            if binPath:
+                if not oldestShellDirAtime or os.path.getatime(binPath) < oldestShellDirAtime:
+                    oldestShellDir = os.path.join(path, shellDir)
+        
+        if oldestShellDir:
+            shutil.rmtree(oldestShellDir)
+        
+def getFreeSpacePercentage(path):
+    statvfs = os.statvfs(path)
+    pfree = (statvfs.f_bavail * statvfs.f_frsize) / float((statvfs.f_blocks * statvfs.f_frsize))
+    return pfree
+    
+if __name__ == "__main__":
+    sys.exit(main())
